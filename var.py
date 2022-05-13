0f92eced31b235419cab84687e6705446a5c8d5a
Ayton Huang <a22980688@gmail.com>
2022-05-13 21:09:19 +0800

    first commit

---
+++ var.py
@@ -0,0 +1,53 @@
+#print(印出)
+#cd = (chang directory) 换资料夹
+print("你好阿!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#宣告(创作)
+#declare(宣告)：创作出变数的意思，只有第一次遇到是宣告
+#variable(变数)：只是一个"名字"，可以想成标签，用来装著/对应到 某个值(value)
+#assignment(设值指令)：遇到等号（＝）就是设值指令，把右边存到左边的意思
+#
+#资料型态（data types）
+#1.整数：int
+#2.浮点数（小数）：float
+#3.布林值：boolean、bool (True，False)  True可理解为对的、真的  False可理解为错的、假的
+#4.字串：string str (文字)  例如 print("Ayton")  由单、双引号包裹其中
+#
+#input(输入)：下指令 input("设定一个条件给使用者看：")  例如： input("请输入名字： ")  重点：型态为字串，要记得单双引号的撰写
+#name = input("请输入您的姓名： ")
+#print("您好，" , name)
+
+#操作GitHub将创建的.py档案上传
+#登入帐号后，执行 New repository 建立新专案
+#开启cmd cd到资料夹 再执行以下指令
+
+#echo "# two" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/ztbh004713/hi.git
+#git push -u origin main
+
+#后续此专案要继续新增档案时，只需输入三行指令
+#git add (需入需要添加的档案)
+#git commit -m "每次上传可添加注解"
+#git push -u origin main 上传档案
